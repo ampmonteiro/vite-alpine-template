b98ab670780143870556a404ffeb82187525b2d1
ampm <>
2023-03-03 21:33:33 +0000

    initial commit: vite template for  alpinejs

---
+++ src/counter.js
@@ -0,0 +1,7 @@
+export const counter = () => ({
+  count: 0,
+
+  increment() {
+    this.count++;
+  },
+});
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Alpine from 'alpinejs';
+import './style.css';
+// import { counter } from './counter';
+
+window.Alpine = Alpine;
+
+// alternative way
+// Alpine.data('counter', counter);
+
+Alpine.start();
